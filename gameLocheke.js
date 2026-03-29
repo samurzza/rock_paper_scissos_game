8e137b9333b0466de9be058564830a4f3928d9f4
abdulaziz samur <aziz5522936989@gmail.com>
2026-03-29 13:11:43 +0300

    step 6 day 6 70% confirm

--- gameLocheke.js
+++ gameLocheke.js
@@ -10,33 +10,75 @@ let theChoicess = document.querySelectorAll(`.theChoice`);
 
 let img = document.querySelectorAll(`.theChoice img`);
 
-let fromRobotChoice = [
+let fromChoice = [
   { className: `paperPlay`, imgSrc: img[0].src },
   { className: `scissorsPlay`, imgSrc: img[1].src },
   { className: `rockPlay`, imgSrc: img[2].src },
 ];
 
 let playerChoice = ``;
+let imgChoice = ``;
+
 let roboteChoice = ``;
 
 let game = document.querySelector(`.game`);
-
-
 
 theChoicess.forEach((choiceEl) => {
   choiceEl.addEventListener("click", (e) => {
     // اخفاء باقي العناصر
     theChoicess.forEach((el) => {
-      if (el !== e.currentTarget) {
-        el.classList.add("hide");
+      el.classList.add("hide");
+
+      if (e.currentTarget.classList[2] === `roce`) {
+        return (playerChoice = `rockPlay`);
+      } else if (e.currentTarget.classList[2] === `scissors`) {
+        return (playerChoice = `scissorsPlay`);
+      } else if (e.currentTarget.classList[2] === `paper`) {
+        return (playerChoice = `paperPlay`);
+      }
+
+      if (e.currentTarget.classList[2] === `rock`) {
+        return (imgChoice = fromChoice[2].imgSrc);
+      } else if (e.currentTarget.classList[2] === `scissors`) {
+        return (imgChoice = fromChoice[1].imgSrc);
+      } else if (e.currentTarget.classList[2] === `paper`) {
+        return (imgChoice = fromChoice[0].imgSrc);
       }
     });
-
     // تكبير وتحريك العنصر المختار
-    e.currentTarget.classList.add("hide");
-
     game.style.opacity = 1;
 
-    console.log(img)
+    let player = document.createElement(`div`);
+    player.setAttribute(
+      `class`,
+      `playert ${
+        e.currentTarget.classList.contains("rock")
+          ? "rockPlay"
+          : e.currentTarget.classList.contains("paper")
+            ? "paperPlay"
+            : e.currentTarget.classList.contains("scissors")
+              ? "scissorsPlay"
+              : ""
+      }`,
+    );
+
+    let playerChoiceImg = document.createElement(`img`);
+    playerChoiceImg.setAttribute(
+      "src",
+      e.currentTarget.classList.contains("rock")
+        ? "/images/icon-rock.svg"
+        : e.currentTarget.classList.contains("paper")
+          ? "/images/icon-paper.svg"
+          : e.currentTarget.classList.contains("scissors")
+            ? "/images/icon-scissors.svg"
+            : "",
+    );
+
+    player.appendChild(playerChoiceImg);
+
+    game.appendChild(player);
+
+    console.log(playerChoice);
+    console.log(img);
   });
 });
