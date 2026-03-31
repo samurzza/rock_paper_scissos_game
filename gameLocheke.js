5c4c1a30b83ef649df1b1810150351a8f0474c94
abdulziz <aziz5522936989@gmail.com>
2026-03-31 15:37:44 +0300

    step 8 day 8 90% completite

--- gameLocheke.js
+++ gameLocheke.js
@@ -10,7 +10,16 @@ let theChoicess = document.querySelectorAll(`.theChoice`);
 
 let img = document.querySelectorAll(`.theChoice img`);
 
-let num = Math.floor(Math.random() * 3);
+
+let triangle = document.querySelector(`.triangle`);
+
+let resetGameH4 = document.querySelector(`.resetGame h4`);
+
+let resetGameResety = document.querySelector(`.resetGame .resety`);
+
+let scoreSpane = document.querySelector(`.score span`);
+
+
 
 let fromChoice = [
   { className: `paperPlay`, imgSrc: img[0].src },
@@ -23,14 +32,22 @@ let texe = document.querySelector(`.texe`);
 
 theChoicess.forEach((choiceEl) => {
   choiceEl.addEventListener("click", (e) => {
+
+    let num = Math.floor(Math.random() * 3);
+ 
     // اخفاء باقي العناصر
     theChoicess.forEach((el) => {
       el.classList.add("hide");
-
+      
     });
+    
+    
     // تكبير وتحريك العنصر المختار
     game.style.opacity = 1;
+    triangle.style.zIndex = `-1`;
+    triangle.style.opacity = 0;
 
+    
     let player = document.createElement(`div`);
     player.setAttribute(
       `class`,
@@ -59,7 +76,7 @@ theChoicess.forEach((choiceEl) => {
 
     player.appendChild(playerChoiceImg);
 
-    game.appendChild(player);
+    game.prepend(player);
 
     let robot = document.createElement(`div`);
     robot.setAttribute(`class`, `robott`);
@@ -72,24 +89,90 @@ theChoicess.forEach((choiceEl) => {
 
     let robotChoiceImg = document.createElement(`img`);
 
-  let x =   setInterval( ()=>{
-    p.textContent = p.textContent - 1;
-    if(+p.textContent === 0){
-      p.textContent = ``;
-      clearInterval(x);
-    }
-  },1000)
+let count = 3;
+
+let x = setInterval(() => {
+  count--;
+  p.textContent = count;
+
+  if (count === 0) {
+    p.textContent = "";
+    clearInterval(x);
+  }
+}, 1000);
   
   setTimeout(()=>{
     robot.setAttribute(`class`, `robot ${fromChoice[num].className}`);
     robotChoiceImg.setAttribute("src", fromChoice[num].imgSrc);
   },3000)
 
+
+  let result = ``
+
+
+
+function GameResult(){
+  if(result === `noWiner`){
+    resetGameH4.textContent = "NO WINER";
+    resetGameResety.classList.remove(`hide`);
+    console.log(scoreSpane); 
+  } else if(result === `winer`){
+    resetGameH4.textContent = "YOU WINER";
+    console.log(scoreSpane);
+    scoreSpane.textContent = +scoreSpane.textContent + 1 
+    resetGameResety.classList.remove(`hide`);
+  } else if (result === `lose`){
+    console.log(scoreSpane); 
+    resetGameH4.textContent = "YOU LOSE";
+    resetGameResety.classList.remove(`hide`);
+  }
+}
+
+
+
+  setTimeout(()=>{
+    if (
+      (player.classList.contains(`paperPlay`) &&
+        robot.classList.contains(`paperPlay`)) ||
+      (player.classList.contains(`scissorsPlay`) &&
+        robot.classList.contains(`scissorsPlay`)) ||
+      (player.classList.contains(`rockPlay`) &&
+        robot.classList.contains(`rockPlay`))
+    ) {
+      result = `noWiner`;
+      return GameResult();
+    } else if (
+      (player.classList.contains(`paperPlay`) &&
+        robot.classList.contains(`scissorsPlay`)) ||
+      (player.classList.contains(`scissorsPlay`) &&
+        robot.classList.contains(`rockPlay`)) ||
+      (player.classList.contains(`rockPlay`) &&
+        robot.classList.contains(`paperPlay`))
+    ) {
+      result = `lose`;
+      return GameResult();
+    } else if (
+      (player.classList.contains(`paperPlay`) &&
+        robot.classList.contains(`rockPlay`)) ||
+      (player.classList.contains(`scissorsPlay`) &&
+        robot.classList.contains(`paperPlay`)) ||
+      (player.classList.contains(`rockPlay`) &&
+        robot.classList.contains(`scissorsPlay`))
+    ) {
+      result = `winer`;
+      return GameResult();
+    }
+  },3300)
+
+
+
   let divtextOne = document.createElement(`div`);
   divtextOne.appendChild(document.createTextNode(`You Picked`))
   divtextOne.style.marginLeft = `30px`
   let divtextTwo = document.createElement(`div`);
   divtextTwo.appendChild(document.createTextNode(`The House Picked`));
+
+  texe.textContent = "";
 
   texe.appendChild(divtextOne);
   texe.appendChild(divtextTwo);
@@ -98,5 +181,27 @@ theChoicess.forEach((choiceEl) => {
 
   game.appendChild(robot);
 
+  
+});
+});
+
+
+
+
+
+
+resetGameResety.addEventListener(`click`, ()=>{
+  document.querySelectorAll(".playert, .robot").forEach((el) => el.remove());
+  texe.textContent = "";
+  resetGameH4.textContent = ``;
+  resetGameResety.classList.add(`hide`)
+
+  game.style.opacity = 0;
+  triangle.style.zIndex = `0`;
+  triangle.style.opacity = 1;
+
+  theChoicess.forEach((el) => {
+    el.classList.remove("hide");
   });
 });
+
